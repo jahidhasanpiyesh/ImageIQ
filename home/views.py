f94fef65e897c9ef247eeb25ae46d11b25ea8ff2
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2025-05-27 09:45:42 +0600

    Update in my code

--- home/views.py
+++ home/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from .forms import ImageForm
 # Create your views here.
 def home(req):
-    form = ImageForm()
-    return render(req, 'home/home.html', {'form': form})
+    if req.method == 'POST':
+        form = ImageForm(req.POST, req.FILES)
+        if form.is_valid():
+            form.save()
+            form = ImageForm()
+    else:
+        form = ImageForm() 
+    return render(req, 'home/home.html', {'form': form}) 
