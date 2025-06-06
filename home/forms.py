1f8488d91ddc8aa18644e9978aaa7c4f8298b731
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2025-06-06 15:33:45 +0600

    Update in this Project & update html indexing file or forms file.

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,26 @@
 from django import forms
 from .models import Image
 
+# class ImageForm(forms.ModelForm):
+#     class Meta:
+#         model = Image
+#         fields = '__all__'
+#         labels = {"photo":""}
+
+
+# gallery/forms.py
+
+from django import forms
+from .models import Image
+
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = '__all__'
-        labels = {"photo":""}
+        fields = '__all__' # Include all fields from the image model.
+        labels = {"photo": ""} # Custom Lable for the photo field.
+        
+        # Customizing the widgets for better UI.
+        widgets = {
+            'photo': forms.ClearableFileInput(attrs={'class': 'form-control'}), 
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+        }
