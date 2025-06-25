b93ca232c28941155830e9a429ff81b9bf64a3dc
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2025-06-25 08:39:02 +0600

    Sign-up system Update and added new massage system.

--- home/views.py
+++ home/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import ImageForm
-from .models import Image
-from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.models import User
 from django.contrib import messages
+from .forms import ImageForm
+from .models import Image
 # Create your views here.
 def home(req):
     if req.method == 'POST':
@@ -19,5 +18,17 @@ def login(req):
     return render(req, 'home/login.html')
 
 def signup(req):
+    if req.method == 'POST':
+        E_mail = req.POST.get('email') # Get the email from the POST request. 
+        pass1 = req.POST.get('password1') # Get the first password from the POST request.
+        pass2 = req.POST.get('password2') # Get the second password from the POST request.
+        if pass1 != pass2: # Check if the two passwords match.
+            messages.error(req, 'Passwords do not match!') # If the passwords do not match, show an error message.
+            return redirect('signup')
+        else:    
+            my_user = User.objects.create_user(E_mail,pass1,pass2) # Create a new user with the provided email and password.
+            my_user.save() # Save the new user to the database.
+            messages.success(req, 'Account created successfully!') # Show a success message.
+        return redirect('login')
     return render(req, 'home/signup.html')
-    
+
