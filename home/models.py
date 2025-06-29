3969406803e910dd76957ace4ef0ebb7b9f05e84
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2025-06-29 09:17:14 +0600

    Photo Upload system base category system added by model.py.

--- home/models.py
+++ home/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+    
 class Image(models.Model):
     photo = models.ImageField(upload_to='myimages') # Field to store the image file, uploaded to 'myimages' directory.
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
     date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.category.name} - {self.photo.name}"
     
     
